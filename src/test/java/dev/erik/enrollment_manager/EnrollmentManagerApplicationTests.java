316c62027a1bcfc6a587d425eb1cce26cf2d0027
Erik-Arch-PC <erikpdn1@gmail.com>
2025-07-27 23:41:17 -0300

    first commit

---
+++ src/test/java/dev/erik/enrollment_manager/EnrollmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erik.enrollment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
